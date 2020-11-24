4e6117ca5379e81a55487910fce78687d0b74aa6
subhiksha-khoros <vsubhiksha1998@gmail.com>
2020-11-24 20:46:53 +0530

    added rollingfile appender log

--- src/main/java/com/example/twitterApi/TwitterApiApplication.java
+++ src/main/java/com/example/twitterApi/TwitterApiApplication.java
@@ -1,13 +1,25 @@
 package com.example.twitterApi;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 public class TwitterApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TwitterApiApplication.class, args);
+	static{
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+		System.setProperty("current.date.time", dateFormat.format(new Date()));
 	}
 
+	static Logger log = Logger.getLogger(TwitterApiApplication.class.getName());
+
+	public static void main(String[] args) {
+		log.info("application started");
+		SpringApplication.run(TwitterApiApplication.class, args);
+		log.warn("stopped");
+	}
 }
